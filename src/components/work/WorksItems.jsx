bd8c29227d169064770677af779e15d0df4f493a
Dorian Raygoza <dorianra@icloud.com>
2023-03-22 01:37:09 -0600

    update the portfolio section

--- src/components/work/WorksItems.jsx
+++ src/components/work/WorksItems.jsx
@@ -4,7 +4,8 @@ const WorksItems = ({item}) => {
   return (
     <div className="work__card" key={item.id}>
         <img src={item.img} alt="" className="work__img" />
-        <h3 className="work__title">{item.title}</h3>
+        <h3 className="work__title">{item.title} | <span className="work__subtitle">{item.category}</span> </h3>
+        
         <a href=" " className="work__button">
             View <i className="bx bx-right-arrow-alt work__button-icon"></i>
         </a>
